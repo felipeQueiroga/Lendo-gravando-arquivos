90b7e6b7b0bcfba0070f54b0b0dddd6f77c8c98a
Felipe Queiroga <luisfelipequeiroga@hotmail.com>
2020-09-29 08:43:15 -0400

    Lendo e gravando um arquivo txt

---
+++ Aula arquivos/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Aula_arquivos
+{
+    class Produto
+    {
+        public string nome { get; set; }
+        public double preco { get; set; }
+        public int qtd { get; set; }
+
+        public Produto(string nome, double preco, int qtd)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.qtd = qtd;
+        }
+
+        public double valorTotal()
+        {
+            return preco * qtd;
+        }
+
+        public override string ToString()
+        {
+            return nome
+                + ","
+                + valorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        
+    }
+}
---
+++ Aula arquivos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Aula_arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Temp\itens.txt";
+            string targetFolderPath = Path.GetDirectoryName(sourcePath) + "\\out";
+            string targetPath = "";
+            string outFileName = "summary.csv";
+
+            List<Produto> lista = new List<Produto>();
+
+            try
+            {
+                //lendo as linhas
+                string[] linhas = File.ReadAllLines(sourcePath);
+                foreach (string l in linhas)
+                {
+                    string[] dados = l.Split(",");
+                    string nome = dados[0];
+                    double preco = double.Parse(dados[1], CultureInfo.InvariantCulture);
+                    int qtd = int.Parse(dados[2]);
+
+                    Produto aux = new Produto(nome, preco, qtd);
+
+                    lista.Add(aux);
+                }
+
+                //gera sub diretorio
+                Directory.CreateDirectory(targetFolderPath);
+                targetPath = targetFolderPath + "\\" + outFileName;
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+
+                    //grava arquivo no subDiretorio
+                    {
+                        foreach (Produto p in lista)
+                        {
+                            sw.WriteLine(p);
+                        }
+                    }
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
